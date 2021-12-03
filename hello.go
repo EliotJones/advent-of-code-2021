7045c56896b2373ba7baff06233c1be83d1a6200
Eliot Jones <elioty@hotmail.co.uk>
2021-12-02 20:49:03 -0400

    day 1 part 2

--- hello.go
+++ hello.go
@@ -7,26 +7,47 @@ import (
 	"strconv"
 )
 
+func scannerForFile(path string) (*bufio.Scanner, error) {
+	if file, err := os.Open(path); err == nil {
+		return bufio.NewScanner(file), nil
+	} else {
+		return nil, err
+	}
+}
+
 func main() {
-	file, err := os.Open("day1.txt")
+	scanner, err := scannerForFile("day1.txt")
 	if err != nil {
-		fmt.Println("Failed to open the file", err)
+		fmt.Println("Failed to scan file", err)
 		os.Exit(1)
 	}
 
+	var index = 0
+	var preceding [3]int
+	var windowSum, prevSum int
 	changeCount := 0
-	prev := -1
-	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		val, err := strconv.Atoi(scanner.Text())
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(2)
 		}
-		if prev >= 0 && val > prev {
-			changeCount++
+
+		if index < 3 {
+			preceding[index] = val
+			windowSum += val
+		} else {
+			replacementIndex := index % 3
+			prevSum = windowSum
+			windowSum = windowSum - preceding[replacementIndex] + val
+			preceding[replacementIndex] = val
+
+			if windowSum > prevSum {
+				changeCount++
+			}
 		}
-		prev = val
+
+		index++
 	}
 
 	fmt.Println(changeCount)
