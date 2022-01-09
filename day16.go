8855c17c0f9b347ca5db859b44390ff6e8e1e479
Eliot Jones <elioty@hotmail.co.uk>
2022-01-09 17:50:55 +0000

    day 16 part 2

--- day16.go
+++ day16.go
@@ -11,6 +11,7 @@ type day16Packet struct {
 	lengthType      int
 	headerBitLength int
 	version         int
+	typeId          int
 }
 
 const day16RootType = 0
@@ -27,6 +28,35 @@ func intSliceSum(s []int) int {
 	}
 
 	return result
+}
+
+func NewRootPacket() *day16Packet {
+	return &day16Packet{
+		packetType: day16RootType,
+		bitLength:  0,
+		children:   make([]*day16Packet, 0),
+	}
+}
+
+func NewOperatorPacket(version int, length int, lengthType int, lengthFieldBitLength int, typeId int) *day16Packet {
+	return &day16Packet{
+		packetType:      day16OperatorType,
+		children:        make([]*day16Packet, 0),
+		bitLength:       0,
+		headerBitLength: 6 + lengthFieldBitLength,
+		length:          length,
+		lengthType:      lengthType,
+		version:         version,
+		typeId:          typeId,
+	}
+}
+
+func NewLiteralPacket(version int) *day16Packet {
+	return &day16Packet{
+		packetType: day16LiteralType,
+		bitLength:  6,
+		version:    version,
+	}
 }
 
 func readOperatorLengthTypeAndValueAndBitsRead(ps *packetStream) (int, int, int) {
@@ -70,17 +100,106 @@ func recursivelyGatherVersions(packet *day16Packet, versions *[]int) {
 	}
 }
 
-func day16(input string) []int {
+/*
+   Packets with type ID 0 are sum packets - their value is the sum of the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+   Packets with type ID 1 are product packets - their value is the result of multiplying together the values of their sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+   Packets with type ID 2 are minimum packets - their value is the minimum of the values of their sub-packets.
+   Packets with type ID 3 are maximum packets - their value is the maximum of the values of their sub-packets.
+   Packets with type ID 5 are greater than packets - their value is 1 if the value of the first sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+   Packets with type ID 6 are less than packets - their value is 1 if the value of the first sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+   Packets with type ID 7 are equal to packets - their value is 1 if the value of the first sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0. These packets always have exactly two sub-packets.
+*/
+func interpretOperation(packet *day16Packet) int {
+	if packet.packetType == day16LiteralType {
+		return packet.literalValue
+	}
+
+	if packet.packetType == day16RootType {
+		if len(packet.children) > 1 {
+			panic("multiple root children?")
+		}
+
+		return interpretOperation(packet.children[0])
+	}
+
+	var result int
+	// Sum type
+	if packet.typeId == 0 {
+		for _, child := range packet.children {
+			result += interpretOperation(child)
+		}
+
+		return result
+	}
+
+	if len(packet.children) == 0 {
+		return 0
+	}
+
+	// Product type
+	if packet.typeId == 1 {
+		result = 1
+		for _, child := range packet.children {
+			result *= interpretOperation(child)
+		}
+
+		return result
+	}
+
+	// Min and max
+	if packet.typeId == 2 || packet.typeId == 3 {
+		result = interpretOperation(packet.children[0])
+
+		for i, child := range packet.children {
+			if i == 0 {
+				continue
+			}
+
+			val := interpretOperation(child)
+			if packet.typeId == 2 && val < result {
+				result = val
+			} else if packet.typeId == 3 && val > result {
+				result = val
+			}
+		}
+
+		return result
+	}
+
+	// Bools
+	if packet.typeId >= 5 && packet.typeId <= 7 {
+		val1 := interpretOperation(packet.children[0])
+		val2 := interpretOperation(packet.children[1])
+
+		if packet.typeId == 5 && val1 > val2 {
+			return 1
+		} else if packet.typeId == 5 {
+			return 0
+		}
+
+		if packet.typeId == 6 && val1 < val2 {
+			return 1
+		} else if packet.typeId == 6 {
+			return 0
+		}
+
+		if val1 == val2 {
+			return 1
+		}
+
+		return 0
+	}
+
+	return 0
+}
+
+func day16(input string) ([]int, *day16Packet) {
 	fmt.Println("Running for input", input)
 	ps := NewPacketStream(input)
 
 	states := make(genericStack, 0)
 
-	states.Push(&day16Packet{
-		packetType: day16RootType,
-		bitLength:  0,
-		children:   make([]*day16Packet, 0),
-	})
+	states.Push(NewRootPacket())
 
 	endOfStream := false
 	for {
@@ -112,13 +231,7 @@ func day16(input string) []int {
 			}
 
 			if packetType == 4 {
-				states.Push(&day16Packet{
-					packetType: day16LiteralType,
-					bitLength:  6,
-					length:     -1,
-					lengthType: -1,
-					version:    int(packetVersion),
-				})
+				states.Push(NewLiteralPacket(int(packetVersion)))
 			} else {
 				lengthType, length, bits := readOperatorLengthTypeAndValueAndBitsRead(ps)
 
@@ -126,39 +239,38 @@ func day16(input string) []int {
 					break
 				}
 
-				states.Push(&day16Packet{
-					packetType:      day16OperatorType,
-					children:        make([]*day16Packet, 0),
-					bitLength:       0,
-					headerBitLength: 6 + bits,
-					length:          length,
-					lengthType:      lengthType,
-					version:         int(packetVersion),
-				})
+				states.Push(NewOperatorPacket(int(packetVersion), length, lengthType, bits, int(packetType)))
 			}
 
 			continue
 		}
 
 		if packet.packetType == day16LiteralType {
-			// Read 5 bit chunks until end
+			var val int
+
+			// Read 5 bit chunks until end, signalled by first bit being 0.
 			for {
 				chunk := ps.readBits(5)
 
 				if chunk == -1 {
+					// If encountering end of stream in some content then exit removing this incomplete node.
 					endOfStream = true
 					break
 				}
+
+				val = (val << 4) + (int(chunk) & 0b1111)
 
 				isLast := (chunk >> 4 & 1) == 0
 				packet.bitLength += 5
 
 				if isLast {
+					packet.literalValue = val
 					break
 				}
 			}
 
 			if endOfStream {
+				// Remove the incomplete node prior to exiting.
 				states.Pop()
 				break
 			}
@@ -167,10 +279,12 @@ func day16(input string) []int {
 		}
 	}
 
-	versions := make([]int, 0)
-	recursivelyGatherVersions(states.Peek().(*day16Packet), &versions)
+	root := states.Peek().(*day16Packet)
 
-	return versions
+	versions := make([]int, 0)
+	recursivelyGatherVersions(root, &versions)
+
+	return versions, root
 }
 
 func day16p1() {
@@ -182,9 +296,18 @@ func day16p1() {
 	scanner.Scan()
 	line := scanner.Text()
 
-	versions := day16(line)
+	versions, root := day16(line)
 
 	result := intSliceSum(versions)
+	part2Result := interpretOperation(root)
 
-	fmt.Println("Result is", result)
+	fmt.Println("Result p1 is", result, "p2 is", part2Result)
+}
+
+func day16p2Input(input string) int {
+	_, root := day16(input)
+
+	result := interpretOperation(root)
+
+	return result
 }
--- day16_test.go
+++ day16_test.go
@@ -19,28 +19,28 @@ func intSlicesEqual(s1 []int, s2 []int) bool {
 }
 
 func TestDay16One(t *testing.T) {
-	versions := day16("D2FE28")
+	versions, _ := day16("D2FE28")
 	if !intSlicesEqual(versions, []int{6}) {
 		t.Errorf("Invalid versions: %x", versions)
 	}
 }
 
 func TestDay16Two(t *testing.T) {
-	versions := day16("38006F45291200")
+	versions, _ := day16("38006F45291200")
 	if !intSlicesEqual(versions, []int{1, 6, 2}) {
 		t.Errorf("Invalid versions: %x", versions)
 	}
 }
 
 func TestDay16Three(t *testing.T) {
-	versions := day16("EE00D40C823060")
+	versions, _ := day16("EE00D40C823060")
 	if !intSlicesEqual(versions, []int{7, 2, 4, 1}) {
 		t.Errorf("Invalid versions: %x", versions)
 	}
 }
 
 func TestDay16Four(t *testing.T) {
-	versions := day16("8A004A801A8002F478")
+	versions, _ := day16("8A004A801A8002F478")
 	result := intSliceSum(versions)
 	if result != 16 {
 		t.Errorf("Incorrect version sum %d from %x", result, versions)
@@ -48,7 +48,7 @@ func TestDay16Four(t *testing.T) {
 }
 
 func TestDay16Five(t *testing.T) {
-	versions := day16("620080001611562C8802118E34")
+	versions, _ := day16("620080001611562C8802118E34")
 	result := intSliceSum(versions)
 	if result != 12 {
 		t.Errorf("Incorrect version sum %d from %x", result, versions)
@@ -56,7 +56,7 @@ func TestDay16Five(t *testing.T) {
 }
 
 func TestDay16Six(t *testing.T) {
-	versions := day16("C0015000016115A2E0802F182340")
+	versions, _ := day16("C0015000016115A2E0802F182340")
 	result := intSliceSum(versions)
 	if result != 23 {
 		t.Errorf("Incorrect version sum %d from %x", result, versions)
@@ -64,9 +64,55 @@ func TestDay16Six(t *testing.T) {
 }
 
 func TestDay16Seven(t *testing.T) {
-	versions := day16("A0016C880162017C3686B18A3D4780")
+	versions, _ := day16("A0016C880162017C3686B18A3D4780")
 	result := intSliceSum(versions)
 	if result != 31 {
 		t.Errorf("Incorrect version sum %d from %x", result, versions)
 	}
+}
+
+func assertIntEqual(t *testing.T, expected int, actual int) {
+	if actual != expected {
+		t.Errorf("Result was incorrect, expected %d but got %d", expected, actual)
+	}
+}
+
+func TestDay16PartTwoOne(t *testing.T) {
+	result := day16p2Input("C200B40A82")
+	assertIntEqual(t, 3, result)
+}
+
+func TestDay16PartTwoTwo(t *testing.T) {
+	result := day16p2Input("04005AC33890")
+	assertIntEqual(t, 54, result)
+}
+
+func TestDay16PartTwoThree(t *testing.T) {
+	result := day16p2Input("880086C3E88112")
+	assertIntEqual(t, 7, result)
+}
+
+func TestDay16PartTwoFour(t *testing.T) {
+	result := day16p2Input("CE00C43D881120")
+	assertIntEqual(t, 9, result)
+}
+
+func TestDay16PartTwoFive(t *testing.T) {
+	result := day16p2Input("D8005AC2A8F0")
+	assertIntEqual(t, 1, result)
+}
+
+func TestDay16PartTwoSix(t *testing.T) {
+	result := day16p2Input("F600BC2D8F")
+	assertIntEqual(t, 0, result)
+}
+
+func TestDay16PartTwoSeven(t *testing.T) {
+	result := day16p2Input("9C005AC2F8F0")
+	assertIntEqual(t, 0, result)
+}
+
+func TestDay16PartTwoEight(t *testing.T) {
+	result := day16p2Input("9C0141080250320F1802104A08")
+	assertIntEqual(t, 1, result)
 }
